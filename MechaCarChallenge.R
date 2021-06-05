f6cbf81c6afb7f44bb1202fc49a57a928ed10915
Michael Leggett <mikeleggett25@gmail.com>
2021-06-05 14:31:14 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,12 +2,20 @@
 library(dplyr)
 
 #read file
-MechaCarData <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+MechaCarData <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
 
-#linear regression
+#create linear regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData)
 
-#summary
+#create summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData))
 
 
+#read file
+SuspensionData <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
+
+#create summary table
+total_summary <- SuspensionData %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#create summary table grouped by lot
+lot_summary <- SuspensionData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
