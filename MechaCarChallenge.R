d0a8a0eb21b7683a53ccb9158ad5826914c2e90d
Michael Leggett <mikeleggett25@gmail.com>
2021-06-05 11:21:03 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,13 @@
+#import libraries
+library(dplyr)
+
+#read file
+MechaCarData <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData)
+
+#summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData))
+
+
