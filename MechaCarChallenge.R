c3ebe6d16a03a5cd30efe3d4a3a0c84e3ada3834
Michael Leggett <mikeleggett25@gmail.com>
2021-06-05 16:15:43 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# Linear Regression to Predict MPG
+
 #import libraries
 library(dplyr)
 
@@ -10,7 +12,10 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #create summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarData))
 
+#-----------------------------------------
 
+# Summary Statistics on Suspension Coils
+  
 #read file
 SuspensionData <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors=F)
 
@@ -19,3 +24,17 @@ total_summary <- SuspensionData %>% summarize(Mean=mean(PSI), Median=median(PSI)
 
 #create summary table grouped by lot
 lot_summary <- SuspensionData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#-----------------------------------------
+  
+# T-Tests on Suspension Coils
+
+#t-test for all manufacturing lots
+t.test(SuspensionData$PSI, mu=1500)
+
+#t-test for lot 1
+t.test(subset(SuspensionData$PSI, SuspensionData$Manufacturing_Lot == "Lot1"), mu=1500)
+#t-test for lot 2
+t.test(subset(SuspensionData$PSI, SuspensionData$Manufacturing_Lot == "Lot2"), mu=1500)
+#t-test for lot 3
+t.test(subset(SuspensionData$PSI, SuspensionData$Manufacturing_Lot == "Lot3"), mu=1500)
